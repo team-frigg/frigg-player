6463cfed5f3ed648b8b306fc544abd6899a9e173
Nicolas Brignol <tools@octet-studio.com>
2020-06-25 10:33:15 +0200

    Better multi map

--- frigg-options.js
+++ frigg-options.js
@@ -203,7 +203,7 @@ var friggConfig = {
                 console.error("You must set a map_token in your map template.")
             }
 
-            var theMapId = "map_" + Date.now();
+            var theMapId = "map_" + frigg.currentSceneId;
             element.querySelector('.mapContainer').setAttribute('id', theMapId);
 
             mapboxgl.accessToken = sceneData.map_token[0].content;
